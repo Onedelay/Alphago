639866f3b16a9a83a5ce7bf06347acb0e6d307e1
unknown <jayhyoe9636@naver.com>
2018-02-12 23:07:08 +0900

    18.02.12 주혜 wordgame

--- app/src/main/java/com/alphago/alphago/activity/GameWordActivity.java
+++ app/src/main/java/com/alphago/alphago/activity/GameWordActivity.java
@@ -1,8 +1,6 @@
 package com.alphago.alphago.activity;
 
 import android.content.Intent;
-import android.graphics.Bitmap;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
