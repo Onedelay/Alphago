3ae277cb58479e60f5862295c487e1f37f6d32d1
unknown <jayhyoe9636@naver.com>
2018-02-12 23:42:33 +0900

    18.02.12 주혜 wordgame

---
+++ app/src/main/java/com/alphago/alphago/activity/GameImageActivity.java
@@ -0,0 +1,15 @@
+package com.alphago.alphago.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.alphago.alphago.R;
+
+public class GameImageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game_image);
+    }
+}
