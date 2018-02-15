2b293de13bdbc9590e2b08a064aa287ac8ddf755
unknown <jayhyoe9636@naver.com>
2018-02-15 22:07:25 +0900

    18.02.15 주혜 imggame

--- app/src/main/java/com/alphago/alphago/activity/GameImageActivity.java
+++ app/src/main/java/com/alphago/alphago/activity/GameImageActivity.java
@@ -2,10 +2,13 @@ package com.alphago.alphago.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 import com.alphago.alphago.R;
 
 public class GameImageActivity extends AppCompatActivity {
+
+    private Button
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
--- app/src/main/java/com/alphago/alphago/activity/GameWordActivity.java
+++ app/src/main/java/com/alphago/alphago/activity/GameWordActivity.java
@@ -12,8 +12,8 @@ import com.alphago.alphago.R;
 
 public class GameWordActivity extends NoStatusBarActivity {
 
-    private Button btn_exit;
-    private Button btn_next;
+    private Button btn_wordgame_exit;
+    private Button btn_wordgame_next;
     private Button btn_wordgame_ex1;
     private Button btn_wordgame_ex2;
     private Button btn_wordgame_ex3;
@@ -24,14 +24,14 @@ public class GameWordActivity extends NoStatusBarActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game_word);
 
-        btn_exit = (Button)findViewById(R.id.btn_wordgame_exit);
-        btn_next = (Button)findViewById(R.id.btn_wordgame_next);
+        btn_wordgame_exit = (Button)findViewById(R.id.btn_wordgame_exit);
+        btn_wordgame_next = (Button)findViewById(R.id.btn_wordgame_next);
         btn_wordgame_ex1 = (Button)findViewById(R.id.btn_wordgame_ex1);
         btn_wordgame_ex2 = (Button)findViewById(R.id.btn_wordgame_ex2);
         btn_wordgame_ex3 = (Button)findViewById(R.id.btn_wordgame_ex3);
         btn_wordgame_ex4 = (Button)findViewById(R.id.btn_wordgame_ex4);
 
-        btn_exit.setOnClickListener(new View.OnClickListener() {
+        btn_wordgame_exit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(GameWordActivity.this, MainActivity.class);
@@ -40,7 +40,7 @@ public class GameWordActivity extends NoStatusBarActivity {
             }
         });
 
-        btn_next.setOnClickListener(new View.OnClickListener() {
+        btn_wordgame_next.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Toast.makeText(getApplicationContext(), "Right or Wrong", Toast.LENGTH_SHORT).show();
