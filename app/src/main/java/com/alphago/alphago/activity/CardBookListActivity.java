181d154e70ce31dc3b9dde6d56ce194d22d8c160
unknown <jayhyoe9636@naver.com>
2018-02-27 08:33:43 +0900

    0227 cardbook design

--- app/src/main/java/com/alphago/alphago/activity/CardBookListActivity.java
+++ app/src/main/java/com/alphago/alphago/activity/CardBookListActivity.java
@@ -4,6 +4,8 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.RecyclerView;
+import android.text.SpannableString;
+import android.text.style.UnderlineSpan;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
