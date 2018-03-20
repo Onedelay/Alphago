ac57af2fc42546072b94ef2014d979b2ee876488
Onedelay <wjy0218@gmail.com>
2018-03-20 12:38:54 +0900

    18.03.20 Image edit(crop, rotate), Loading indication not display(needed AsyncTask)

--- app/src/main/java/com/alphago/alphago/activity/SendImageActivity.java
+++ app/src/main/java/com/alphago/alphago/activity/SendImageActivity.java
@@ -3,6 +3,8 @@ package com.alphago.alphago.activity;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Rect;
+import android.net.Uri;
 import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -17,8 +19,11 @@ import com.alphago.alphago.api.AlphagoServer;
 import com.alphago.alphago.dto.ResponeImageLabel;
 import com.alphago.alphago.fragment.ImageSelectionMethodDialog;
 import com.squareup.picasso.Picasso;
+import com.theartofdev.edmodo.cropper.CropImageView;
 
+import java.io.ByteArrayOutputStream;
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.Serializable;
 
 import retrofit2.Call;
@@ -32,6 +37,7 @@ public class SendImageActivity extends NoStatusBarActivity {
     private String max_label;
     private int ID;
     private int cate_ID;
+    private CropImageView cropImageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,14 +45,21 @@ public class SendImageActivity extends NoStatusBarActivity {
         setContentView(R.layout.activity_send_image);
 
         imageFile = (File) getIntent().getSerializableExtra("sendImage");
-        if (imageFile.exists()) {
-            ImageView myImage = (ImageView) findViewById(R.id.sendImageView);
-            Picasso.with(getBaseContext())
-                    .load(imageFile)
-                    .centerInside()
-                    .fit()
-                    .into(myImage);
-        }
+
+        cropImageView = (CropImageView) findViewById(R.id.cropImageView);
+        cropImageView.setImageUriAsync(Uri.fromFile(imageFile));
+        cropImageView.setAspectRatio(1, 1);
+        cropImageView.setFixedAspectRatio(true);
+        cropImageView.setCropShape(CropImageView.CropShape.RECTANGLE);
+        cropImageView.setAutoZoomEnabled(false);
+        cropImageView.setCropRect(new Rect(1000, 1000, 1000, 1000));
+
+        findViewById(R.id.crop_image_menu_rotate_right).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                cropImageView.rotateImage(90);
+            }
+        });
 
         findViewById(R.id.btn_retry).setOnClickListener(new View.OnClickListener() {
             @Override
@@ -59,12 +72,45 @@ public class SendImageActivity extends NoStatusBarActivity {
             @Override
             public void onClick(View v) {
                 frameLoading.setVisibility(View.VISIBLE);
+
+//                new Thread(new Runnable() {
+//                    @Override
+//                    public void run() {
+//                        frameLoading.post(new Runnable() {
+//                            @Override
+//                            public void run() {
+//                                frameLoading.setVisibility(View.VISIBLE);
+//                            }
+//                        });
+//                    }
+//                }).start();
+
+                cropImageView.setOnCropImageCompleteListener(new CropImageView.OnCropImageCompleteListener() {
+                    @Override
+                    public void onCropImageComplete(CropImageView view, CropImageView.CropResult result) {
+                    }
+                });
+                cropImageView.getCroppedImageAsync();
+
+                Bitmap bitmap = cropImageView.getCroppedImage();
+                try{
+                    ByteArrayOutputStream bos = new ByteArrayOutputStream();
+                    bitmap.compress(Bitmap.CompressFormat.JPEG, 100, bos);
+                    byte[] bitmapData = bos.toByteArray();
+
+                    FileOutputStream fos = new FileOutputStream(imageFile);
+                    fos.write(bitmapData);
+                    fos.flush();
+                    fos.close();
+                } catch (Exception e){
+                    e.printStackTrace();
+                }
+
                 AlphagoServer.getInstance().sendImage(getBaseContext(), imageFile, new Callback<ResponeImageLabel>() {
                     @Override
                     public void onResponse(Call<ResponeImageLabel> call, Response<ResponeImageLabel> response) {
                         frameLoading.setVisibility(View.GONE);
                         if (response.body() != null) {
-                            //Toast.makeText(SendImageActivity.this, response.body().getResponseLabel(), Toast.LENGTH_SHORT).show();
                             category = response.body().getCategory();
                             max_label = response.body().getResponseLabel();
                             ID = response.body().getID();
