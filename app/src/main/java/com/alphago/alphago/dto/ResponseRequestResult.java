0cb042dadef7a5aad766be9a8a93b0486202d59c
Onedelay <wjy0218@gmail.com>
2018-04-14 20:03:54 +0900

    18.04.14 누락된 dto

---
+++ app/src/main/java/com/alphago/alphago/dto/ResponseRequestResult.java
@@ -0,0 +1,28 @@
+package com.alphago.alphago.dto;
+
+public class ResponseRequestResult {
+    private String category;
+    private String name;
+    private int ID;
+    private int cate_ID;
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public int getID() {
+        return ID;
+    }
+
+    public int getCate_ID() {
+        return cate_ID;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
